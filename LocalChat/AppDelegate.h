68d37edc32b7fe6861ff139cddba40ac4c58411a
James Stiehl <jstiehl@gs-mbp15-james.local>
2014-11-20 19:11:54 -0800

    Initial Commit

---
+++ LocalChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalChat
+//
+//  Created by James Stiehl on 11/20/14.
+//  Copyright (c) 2014 James Stiehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalChat
+//
+//  Created by James Stiehl on 11/20/14.
+//  Copyright (c) 2014 James Stiehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
